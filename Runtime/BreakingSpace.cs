53f9ab45ffdee63bb501c3edebda678128f27d89
Fallever <piscovip@163.com>
2022-05-05 20:54:52 +0800

    1

---
+++ Runtime/BreakingSpace.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace MioTool
+{
+    [RequireComponent(typeof(Text))]
+    public class BreakingSpace : MonoBehaviour
+    {
+        public static readonly string no_breaking_space = "\u00A0";
+
+        protected Text mytext;
+
+        private void Awake()
+        {
+            mytext = GetComponent<Text>();
+            mytext.RegisterDirtyVerticesCallback(SetMyText);
+        }
+
+        public void SetMyText()
+        {
+            if (mytext.text.Contains(" "))
+            {
+                mytext.text = mytext.text.Replace(" ", no_breaking_space);
+            }
+        }
+    }
+}
